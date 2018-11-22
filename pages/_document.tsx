55e24c8cc7b3c18cf0038c3fd5a6c45639dc279a
Urban Lavbič <urban@reveel.it>
2018-11-22 13:28:55 +0100

    Change: Make Roboto and Material Icons part of all pages served.

---
+++ pages/_document.tsx
@@ -0,0 +1,34 @@
+import Document, {
+  Head,
+  Main,
+  NextScript,
+  NextDocumentContext
+} from "next/document";
+
+export default class MyDocument extends Document {
+  static async getInitialProps(ctx: NextDocumentContext) {
+    const initialProps = await Document.getInitialProps(ctx);
+    return { ...initialProps };
+  }
+
+  render() {
+    return (
+      <html>
+        <Head>
+          <link
+            rel="stylesheet"
+            href="https://fonts.googleapis.com/css?family=Roboto:300,400,500"
+          />
+          <link
+            rel="stylesheet"
+            href="https://fonts.googleapis.com/icon?family=Material+Icons"
+          />
+        </Head>
+        <body>
+          <Main />
+          <NextScript />
+        </body>
+      </html>
+    );
+  }
+}
